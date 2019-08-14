9f6c792aebc75ad6603902eba2fa186891b121ee
Francesca Vago <vago.francesca@gmail.com>
2019-08-14 18:07:01 +0100

    added and seeded database with flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  Flat.create!(
+    name: Faker::Address.community,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: Faker::Number.within(range: 1..200),
+    number_of_guests: Faker::Number.within(range: 1..6)
+    )
+end
